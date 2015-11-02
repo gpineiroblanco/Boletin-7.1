9ed1afc1c64acc8119ff8c5c6c36cc8013d685e9
Toshiba <Toshiba@192.168.0.18>
2015-11-03 00:59:27 +0100

    Aplicacion terminada.

---
+++ src/boletin7/pkg1/Boletin71.java
@@ -0,0 +1,21 @@
+
+package boletin7.pkg1;
+
+/**
+ *
+ * @author Toshiba
+ */
+public class Boletin71 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Condicion par1=new Condicion();
+        double num=par1.pedirNumero();
+        par1.comparar(num);
+        
+    }
+    
+}
